08b9bae971255805ac45badaea72f5b0b7af0019
Devisrinivas Kandikattu <115813140+DevisrinivasK@users.noreply.github.com>
2025-10-13 09:47:17 +0530

    Add files via upload
    Started practicing arrays

---
+++ src/arrays/PrintAlternatives.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class PrintAlternatives {
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,8,9,10,11,12};
+        for(int i=0;i<arr.length;i+=2){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
